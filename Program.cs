439f93400bf5c687c7cb9bf30463f8dfb835aa45
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-10-22 13:28:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿ 
+
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Üçgenin boyutunu girin: ");
+            int size = int.Parse(Console.ReadLine());
+
+            if (size <= 0)
+            {
+                Console.WriteLine("Geçersiz boyut. Pozitif bir tamsayı girin.");
+                return;
+            }
+
+            TriangleDrawer.TriangleDrawer.DrawTriangle(size);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş. Bir tamsayı girin.");
+        }
+    }
+}
---
+++ TriangleDrawer.cs
@@ -0,0 +1,18 @@
+﻿namespace TriangleDrawer
+{
+    class TriangleDrawer
+    {
+        public static void DrawTriangle(int size)
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
